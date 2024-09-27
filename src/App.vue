69ee0710a6295ec668af53e8749def680663da57
beishuang <beishuang.cc@163.com>
2024-09-27 14:58:48 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="common-approval-page">
+      <ComponentA
+          ref="formRef"
+          @event01="event"
+      />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ComponentA from './components/ComponentA.vue';
+
+
+
+const event = (a: string) => {
+
+}
+</script>
---
+++ src/components/ComponentA.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts" setup>
+export interface FormEmits {
+    (e: 'event01', value: string): void
+    (e: 'event02', value: string): void
+    (e: 'event03', value: string): void
+    (e: 'event04', value: string): void
+    (e: 'event05', value: string): void
+    (e: 'event06', value: string): void
+    (e: 'event07', value: string): void
+    (e: 'event08', value: string): void
+    (e: 'event09', value: string): void
+}
+
+defineEmits<FormEmits>()
+</script>
+<style lang="less" scoped></style>
